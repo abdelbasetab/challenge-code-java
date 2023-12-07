de8a2699cf6b540fef894ed08d294c4f94bb5fa0
Abdelabset <Abdelbaset.Abidi@studmail.w-hs.de>
2023-12-07 22:16:00 +0100

    longest Commen Prefix

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,38 @@
 package org.example;
 
 public class Main {
+
+
+    public static String longestCommonPrefix(String[] strs) {
+        int longst_string = 0;
+        int m = 0;
+        int d = 0;
+        for (int i = 1; i < strs.length; i++) {
+
+            m = 0;
+            while(( m < strs[0].length() && (m < strs[i].length())) && strs[0].charAt(m) == strs[i].charAt(m) ){
+
+                if (longst_string <= m && longst_string != 0){
+                    break;
+                }
+                m++;
+            }
+            d = m;// holder for m
+            if (longst_string > d  || longst_string == 0){
+                longst_string =d;
+            }
+
+
+        }
+        return strs[0].substring(0,longst_string);
+    }
+
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] strs = {"flower","flow","flight"} ;
+        String[] strs1 = {"dog","racecar","car"};
+        System.out.println(longestCommonPrefix(strs));
+
     }
 }
