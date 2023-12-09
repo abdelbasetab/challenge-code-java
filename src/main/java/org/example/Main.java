c489cd0096ae68a998d7cc4970ac7478a563ab32
Abdelabset <Abdelbaset.Abidi@studmail.w-hs.de>
2023-12-09 11:14:21 +0100

    longest Commen Prefix isValid

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,41 @@
 package org.example;
 
+import java.util.Stack;
+
 public class Main {
 
 
+  
+    public static boolean isValid(String s) {
+
+
+        int i = 0;
+        Stack<Character> stack = new Stack<>();//FIFO algorithme
+       for (char c : s.toCharArray()){
+
+           if(c == '(' || c == '[' || c == '{'){
+               stack.push(c);
+           } else if (c == ')' && !stack.empty() && stack.peek()=='(') {
+               stack.pop();//delete the last character on the stack
+           }else if (c == ']' && !stack.empty() && stack.peek()=='[') {
+               stack.pop();
+           }else if (c == '}' && !stack.empty() && stack.peek()=='{') {
+               stack.pop();
+           }else
+               return  false;
+       }
+       return stack.empty();
+
+
+    }
+
+
+    public static void main(String[] args) {
+        String strs = "()()" ;
+        String[] strs1 = {"c","acc","ccc"};
+        System.out.println( isValid(strs));
+
+    }
     public static String longestCommonPrefix(String[] strs) {
         int longst_string = 0;
         int m = 0;
@@ -10,6 +43,8 @@ public class Main {
         for (int i = 1; i < strs.length; i++) {
 
             m = 0;
+            if(strs[0].isBlank() ||strs[i].isBlank() ) return "";
+            if(strs[0].charAt(0) != strs[i].charAt(0) ) return "";
             while(( m < strs[0].length() && (m < strs[i].length())) && strs[0].charAt(m) == strs[i].charAt(m) ){
 
                 if (longst_string <= m && longst_string != 0){
@@ -28,11 +63,4 @@ public class Main {
     }
 
 
-
-    public static void main(String[] args) {
-        String[] strs = {"flower","flow","flight"} ;
-        String[] strs1 = {"dog","racecar","car"};
-        System.out.println(longestCommonPrefix(strs));
-
-    }
 }
